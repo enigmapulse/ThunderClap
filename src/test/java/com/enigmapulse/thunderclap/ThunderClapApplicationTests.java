07aba3cf9084728caef8a1608c5d6870fc43a185
enigmapulse <navneetkashyap2006@gmail.com>
2025-02-23 23:09:10 +0530

    project created

---
+++ src/test/java/com/enigmapulse/thunderclap/ThunderClapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigmapulse.thunderclap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderClapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
